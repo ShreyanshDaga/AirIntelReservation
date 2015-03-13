e0c2f9df957857fc2acfbf9093078e1a0ececeef
Shreyansh Daga <sdaga@usc.edu>
2015-03-13 03:24:19 -0700

    Initial Commit, Entity Classes

---
+++ AIR.Entities/Admin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIR.Entities
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PasswordHash { get; set; }
+
+    }
+}
